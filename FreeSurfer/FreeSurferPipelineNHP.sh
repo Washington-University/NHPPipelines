331732d84d4cb7b952250ffae2f5f6bc2f82d6b8
takuya-hayashi <takuya.hayashi@gmail.com>
2021-04-23 23:54:25 +0900

    update white skeleton by multiplying 255

--- FreeSurfer/FreeSurferPipelineNHP.sh
+++ FreeSurfer/FreeSurferPipelineNHP.sh
@@ -326,6 +326,7 @@ function runNormalize2 () {
 		fi
 		fslmaths wmlesion.nii.gz -thr 0 -bin wmlesion_bin.nii.gz # lesion threshold by probability
 		"$PipelineScripts"/MakeDimto1mm.sh $SPECIES wmlesion_bin.nii.gz nn
+		fslmaths wmskeleton_1mm.nii.gz -thr 0.2 -bin -mul 255 wmskeleton_1mm.nii.gz
 		mri_convert -ns 1 -odt uchar wmlesion_bin_1mm.nii.gz wmlesion_bin_1mm_conform.nii.gz --conform
 		fslmaths wmlesion_bin_1mm_conform.nii.gz -mul 110 -max wm.nii.gz wm.nii.gz
 	fi
